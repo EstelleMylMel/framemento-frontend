e6169905ac1e8119fc497b0602fb7c01b2c8fd87
Clarice Bascou <clarice.bascou@gmail.com>
2023-12-12 11:53:32 +0100

    bug babel encours

--- App.tsx
+++ App.tsx
@@ -18,84 +18,85 @@ const CommunityStack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
-const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator initialRouteName="Welcome" screenOptions={{
-      header: (props) => <Header {...props} />, // TROUVER COMMENT IMPORTER LE HEADER
-      drawerActiveTintColor: '#655074',
-      drawerType: 'back',
-      }}>
-      <Drawer.Screen name="Search" component={SearchScreen} />
-      <Drawer.Screen name="My recipes" component={MyRecipesScreen} />
-    </Drawer.Navigator>
-  );
-};
+// const DrawerNavigator = () => {
+//   return (
+//     <Drawer.Navigator initialRouteName="Welcome" screenOptions={{
+//       header: (props) => <Header {...props} />, // TROUVER COMMENT IMPORTER LE HEADER
+//       drawerActiveTintColor: '#655074',
+//       drawerType: 'back',
+//       }}>
+//       <Drawer.Screen name="Search" component={SearchScreen} />
+//       <Drawer.Screen name="My recipes" component={MyRecipesScreen} />
+//     </Drawer.Navigator>
+//   );
+// };
 
-const MyRollsStackNavigation = () => {
-  /// navigation 
-  return (
-    <NavigationContainer>
-     <MyRollsStack.Navigator screenOptions={{ headerShown: false }}>
-      {/* Ecran qui présente toutes les pellicules */}
-       <MyRollsStack.Screen name="Rolls" component={RollsScreen}/> 
-       {/* Ecran ou MODALE ? ajouter / modifier une pellicule  */}
-       {/* <MyRollsStack.Screen name="RollEdit" component={RollEditScreen}/> */}
-       {/* Ecran qui présente toutes les photos de la pellicule sélectionnée */}
-       <MyRollsStack.Screen name="Roll" component={RollScreen}/>
-       {/* Ecran ou MODALE ? ajouter / modifier / consulter une photo  */}
-       {/* Ecran qui présente toutes les photos de la pellicule sélectionnée */}
-     </MyRollsStack.Navigator>
-   </NavigationContainer>
-  )
-}
+// const MyRollsStackNavigation = () => {
+//   /// navigation 
+//   return (
+//     <NavigationContainer>
+//      <MyRollsStack.Navigator screenOptions={{ headerShown: false }}>
+//       {/* Ecran qui présente toutes les pellicules */}
+//        <MyRollsStack.Screen name="Rolls" component={RollsScreen}/> 
+//        {/* Ecran ou MODALE ? ajouter / modifier une pellicule  */}
+//        {/* <MyRollsStack.Screen name="RollEdit" component={RollEditScreen}/> */}
+//        {/* Ecran qui présente toutes les photos de la pellicule sélectionnée */}
+//        <MyRollsStack.Screen name="Roll" component={RollScreen}/>
+//        {/* Ecran ou MODALE ? ajouter / modifier / consulter une photo  */}
+//        {/* Ecran qui présente toutes les photos de la pellicule sélectionnée */}
+//      </MyRollsStack.Navigator>
+//    </NavigationContainer>
+//   )
+// }
 
-const CommunityStackNavigation = () => {
-  /// navigation 
-  return (
-    <NavigationContainer>
-     <CommunityStack.Navigator screenOptions={{ headerShown: false }}>
-       <CommunityStack.Screen name="Welcome" component={WelcomeScreen} />
-       {/* + autres pages */}
-     </CommunityStack.Navigator>
-   </NavigationContainer>
-  )
-}
+// const CommunityStackNavigation = () => {
+//   /// navigation 
+//   return (
+//     <NavigationContainer>
+//      <CommunityStack.Navigator screenOptions={{ headerShown: false }}>
+//        <CommunityStack.Screen name="Welcome" component={WelcomeScreen} />
+//        {/* + autres pages */}
+//      </CommunityStack.Navigator>
+//    </NavigationContainer>
+//   )
+// }
 
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
-        let iconName = '';
+// const TabNavigator = () => {
+//   return (
+//     <Tab.Navigator screenOptions={({ route }) => ({
+//       tabBarIcon: ({ color, size }) => {
+//         let iconName = '';
  
-        if (route.name === 'Home') {
-          iconName = 'home';
-        } else if (route.name === 'Profile') {
-          iconName = 'user';
-        }
+//         if (route.name === 'Home') {
+//           iconName = 'home';
+//         } else if (route.name === 'Profile') {
+//           iconName = 'user';
+//         }
  
-        return <FontAwesome name={iconName} size={size} color={color} />; // ATTENTION CHANGER LES ICONS
-      },
-      tabBarActiveTintColor: '#2196f3',
-      tabBarInactiveTintColor: 'gray',
-      headerShown: false,
-    })}>
+//         return <FontAwesome name={iconName} size={size} color={color} />; // ATTENTION CHANGER LES ICONS
+//       },
+//       tabBarActiveTintColor: '#2196f3',
+//       tabBarInactiveTintColor: 'gray',
+//       headerShown: false,
+//     })}>
 
-      <Tab.Screen name="Mes pellicules" component={MyRollsStackNavigation} />
-      <Tab.Screen name="Communauté" component={CommunityStackNavigation} />
-    </Tab.Navigator>
-  );
- }
+//       <Tab.Screen name="Mes pellicules" component={MyRollsStackNavigation} />
+//       <Tab.Screen name="Communauté" component={CommunityStackNavigation} />
+//     </Tab.Navigator>
+//   );
+//  }
 
 export default function App() {
   return (
-    <NavigationContainer>
-     <AppStack.Navigator screenOptions={{ headerShown: false }}>
-       <AppStack.Screen name="Welcome" component={WelcomeScreen} />
-       <AppStack.Screen name="SignUp" component={SignUpScreen} />
-       <AppStack.Screen name="SignIn" component={SignInScreen} />
-       <AppStack.Screen name="TabNavigator" component={TabNavigator} />
-     </AppStack.Navigator>
-   </NavigationContainer>
+    <View>App</View>
+  //   <NavigationContainer>
+  //    <AppStack.Navigator screenOptions={{ headerShown: false }}>
+  //      <AppStack.Screen name="Welcome" component={WelcomeScreen} />
+  //      <AppStack.Screen name="SignUp" component={SignUpScreen} />
+  //      <AppStack.Screen name="SignIn" component={SignInScreen} />
+  //      <AppStack.Screen name="TabNavigator" component={TabNavigator} />
+  //    </AppStack.Navigator>
+  //  </NavigationContainer>
   );
 }
 
